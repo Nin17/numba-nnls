bf4c7601b3d9dd5d694df93cd787d4267aacb592
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-05-03 19:47:45 +0200

    new function names

--- numba_nnls/__init__.py
+++ numba_nnls/__init__.py
@@ -2,12 +2,11 @@
 
 __version__ = "0.1.0"
 
+__all__ = ("nnls_007_111", "nnls_007_111_", "nnls_112_114", "scipy_overloads")
+
 from . import scipy_overloads
-from ._nnls import nnls_new, nnls_old, nnls_old_
+from ._nnls import nnls_007_111, nnls_007_111_, nnls_112_114
 
 
 def _init() -> None:
     from . import scipy_overloads
-
-
-__all__ = ["nnls_new", "nnls_old", "nnls_old_", "scipy_overloads"]
